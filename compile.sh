e694b98e2f796a59808c0c6bd0c99571392570e9
Andrey Lushnikov <lushnikov@chromium.org>
2015-12-04 18:57:33 -0800

    changes

--- compile.sh
+++ compile.sh
@@ -2,5 +2,7 @@
 files=$(find . -name 'demo*.s?ss')
 for file in $files; do
     cssName="${file%.*}.css";
-    sass --force --style compressed $file:$cssName
+    sass --force --style expanded $file:$cssName
+    chmod 755 $cssName
+    chmod 755 "$cssName".map
 done
